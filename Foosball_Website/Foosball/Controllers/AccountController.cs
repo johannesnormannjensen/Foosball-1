254ec87fb5a1775f26cf0af3dab1c3063b06f561
Johannes Normann Jensen <jnj@iportalen.dk>
2015-06-01 19:43:59 +0200

    top top kek

--- Foosball_Website/Foosball/Controllers/AccountController.cs
+++ Foosball_Website/Foosball/Controllers/AccountController.cs
@@ -161,18 +161,10 @@ namespace Foosball.Controllers
                 if (result.Succeeded)
                 {
 
-                    model.Player = player;
                     player.ApplicationUserId = user.Id;
-                    FoosballDbContext db = new FoosballDbContext();
-                    db.Players.Add(model.Player);
+                    db.Players.Add(player);
                     db.SaveChanges();
                     await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
-
-                    // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
-                    // Send an email with this link
-                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
-                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
                     return RedirectToAction("Index", "Home");
                 }
